aeb8d41087ca68e19751f39c0047698609efee05
AceHotel33 <lomvax@gmail.com>
2024-07-02 15:19:29 +0400

    Added a count characters function

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 frankenstein_path = 'books/frankenstein.txt'
 
-
 def main():
     frankenstein = open_book(frankenstein_path)
     count = count_words(frankenstein)
-    print(count)
+    char_dict = count_chars(frankenstein)
+    print(char_dict)
 
 def open_book(book_path):
     with open(book_path) as f:
@@ -16,4 +16,19 @@ def count_words(book):
     count_words = len(split_book)
     return count_words
 
+def count_chars(string):
+    lower_string = string.lower()
+    char_count = {}
+    for ch in lower_string:
+        if ch not in char_count:
+            char_count[ch] = 1
+        else:
+            char_count[ch] += 1
+    return char_count
+
+def test():
+    test_str = "Hey my name is Vakho!"
+    print(count_chars(test_str))
+
+# test()
 main()
